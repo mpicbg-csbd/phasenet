ba48f5be9e7c53c67e996966ff4ba0d609816b89
Debayan Saha <debayansaha102@gmail.com>
2020-02-20 12:24:05 +0100

    Moved from config

--- phasenet/model.py
+++ phasenet/model.py
@@ -176,12 +176,6 @@ class Config(BaseConfig):
 
         self.n_channel_out = len(random_zernike_wavefront(self.zernike_amplitude_ranges))
 
-        if isinstance(self.crop_params,dict) and 'crop_shape' in self.crop_params:
-            self._model_input_shape = self.crop_params.get('crop_shape')
-        else:
-            self._model_input_shape = self.psf_shape
-
-
 
 class PhaseNet(BaseModel):
     """PhaseNet model.
@@ -226,7 +220,12 @@ class PhaseNet(BaseModel):
 
 
     def _build(self):
-        input_shape = tuple(self.config._model_input_shape) + (self.config.n_channel_in,)
+
+        if isinstance(self.config.crop_params,dict) and 'crop_shape' in self.config.crop_params:
+            _model_input_shape = self.config.crop_params.get('crop_shape')
+        else:
+            _model_input_shape = self.config.psf_shape
+        input_shape = tuple(_model_input_shape) + (self.config.n_channel_in,)
         output_size = self.config.n_channel_out
         kernel_size = self.config.net_kernel_size
         pool_size = self.config.net_pool_size
